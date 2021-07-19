6e6e00879f12781b1227764e87ad275e38686e20
Ryan <rb@determined.ai>
2021-07-19 12:34:49 -0600

    improvements to api.request and Session
    - support a timeout on requests
    - support non-dict-type body (now called 'json')
    - support pre-encoded json body (now called 'data')
    - expose Session as client.Session

--- harness/determined/cli/agent.py
+++ harness/determined/cli/agent.py
@@ -131,7 +131,7 @@ def patch_agent(enabled: bool) -> Callable[[argparse.Namespace], None]:
             headers = {"Content-Type": "application/merge-patch+json"}
             payload = {"enabled": enabled}
 
-            api.patch(args.master, path, body=payload, headers=headers)
+            api.patch(args.master, path, json=payload, headers=headers)
             status = "Disabled" if not enabled else "Enabled"
             print("{} agent {}".format(status, agent_id))
 
@@ -145,7 +145,7 @@ def patch_slot(enabled: bool) -> Callable[[argparse.Namespace], None]:
         headers = {"Content-Type": "application/merge-patch+json"}
         payload = {"enabled": enabled}
 
-        api.patch(args.master, path, body=payload, headers=headers)
+        api.patch(args.master, path, json=payload, headers=headers)
         status = "Disabled" if not enabled else "Enabled"
         print("{} slot {} of agent {}".format(status, args.slot_id, args.agent_id))
 
